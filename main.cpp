8bc22dc36269b946ce5dbbc4729e2d8370827fdf
cjwddtc <leeshengyuan@live.com>
2016-03-25 05:24:03 +0100

    add f()

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <functional>
 #include <map>
 #include <stdio.h>
@@ -44,23 +45,25 @@ unsigned char s_array[8][64] = {
      7,  11, 4,  1, 9,  12, 14, 2,  0,  6,  10, 13, 15, 3,  5,  8,
      1,  15, 13, 8, 10, 3,  7,  4,  12, 5,  6,  11, 0,  14, 9,  2,
      2,  1,  14, 7, 4,  10, 8,  13, 15, 12, 9,  0,  3,  5,  6,  11}};
+unsigned long (*s_box_array)[8];
 
-unsigned char p_box_data[32] = {16, 7, 20, 21, 29, 12, 28, 17, 1,  15, 23,
-                                26, 5, 18, 31, 10, 2,  8,  24, 14, 32, 27,
-                                3,  9, 19, 13, 30, 6,  22, 11, 4,  25};
+std::vector<unsigned char> p_box_data = {
+    16, 7, 20, 21, 29, 12, 28, 17, 1,  15, 23, 26, 5,  18, 31, 10,
+    2,  8, 24, 14, 32, 27, 3,  9,  19, 13, 30, 6,  22, 11, 4,  25};
 unsigned long p_box_array[256][4];
-unsigned char extend_data[48] = {0};
-unsigned long long extend_array[4][256];
+std::vector<unsigned char> extend_data = {
+    32, 1,  2,  3,  4,  5,  4,  5,  6,  7,  8,  9,  8,  9,  10, 11,
+    12, 13, 12, 13, 14, 15, 16, 17, 16, 17, 18, 19, 20, 21, 20, 21,
+    22, 23, 24, 25, 24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32, 1};
+unsigned long long extend_array[256][4];
+
+unsigned char key_shift[16] = {1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1};
+
 namespace no_use {
-template <size_t N> class TYPE {};
-template <> class TYPE<32> { typedef unsigned long type; };
-template <> class TYPE<64> { typedef unsigned long long type; };
-template <> class TYPE<48> { typedef unsigned long long type; };
-template <size_t N> using len_type = typename TYPE<N>::type;
 template <size_t count, size_t block_size, typename in_type, typename out_type,
           size_t count_ = 0>
 struct tran {
-  static inline out_type f(in_type in, out_type (*array)[count]) {
+  static inline out_type f(in_type in, out_type (*&array)[count]) {
     return array[in & ((0x1 << block_size) - 1)][count_] |
            tran<count, block_size, in_type, out_type, count_ + 1>::f(
                in >> block_size, array);
@@ -69,12 +72,11 @@ struct tran {
 
 template <size_t count, size_t block_size, typename in_type, typename out_type>
 struct tran<count, block_size, in_type, out_type, count> {
-  static inline out_type f(in_type in, out_type (*array)[count]) { return 0; };
+  static inline out_type f(in_type in, out_type (*&array)[count]) { return 0; };
 };
 }
 
-template <size_t block_size = 8,
-          typename in_type = no_use::len_type<block_size << 3>, size_t count,
+template <size_t block_size = 8, typename in_type, size_t count,
           typename out_type>
 static inline out_type map(in_type in, out_type (*array)[count]) {
   return no_use::tran<count, block_size, in_type, out_type>::f(in, array);
@@ -100,15 +102,55 @@ static inline void init(std::vector<unsigned char> source,
       }
 }
 
+void init_s() {
+  s_box_array = (unsigned long(*)[8])malloc(sizeof(unsigned long) * 64 * 8);
+  for (unsigned i = 1; i < 8; i++) {
+    for (unsigned j = 0; j < 64; j++) {
+      s_box_array[j][i] = s_array[i][j] << 4 * i;
+    }
+  }
+}
+
+void init() {
+  init_s();
+  std::for_each(
+      p_box_data.begin(), p_box_data.end(),
+      std::bind(std::minus<unsigned char>(), std::placeholders::_1, 1));
+  std::for_each(
+      extend_data.begin(), extend_data.end(),
+      std::bind(std::minus<unsigned char>(), std::placeholders::_1, 1));
+  init(p_box_data, p_box_array);
+  init(extend_data, extend_array);
+}
+
+unsigned long f(unsigned long value, unsigned long long key) {
+  return map(map<6>(map(value, extend_array) ^ key, s_box_array), p_box_array);
+}
+
+unsigned long long des(unsigned long long value, unsigned long long key) {
+  unsigned long left = value;
+  unsigned long right = value >> 32;
+  unsigned long tmp;
+  tmp = left;
+  left = right;
+  right = tmp ^ f(right, key);
+}
+
+void gen_sub_key(unsigned long long key, unsigned long long keys[16]) {
+  unsigned long long left = key << 32;
+  left |= left >> 32;
+  unsigned long long right = key >> 32;
+  right |= right << 32;
+  for (unsigned i = 0; i < 16; i++) {
+  }
+}
+
 int main() {
-  unsigned long n;
   unsigned long m;
-  unsigned long array[256][4] = {{0}};
-  std::vector<unsigned char> qwe;
-  init(qwe, array);
+  init();
   clock_t a = clock();
   for (unsigned long n = 0; n < 0xfffffff; n++)
-    m += map(n, array);
+    m += f(n, 0x1);
   clock_t b = clock();
   printf("%u", b - a, m);
 }
